d00f2862fa77fb2278a39b44c31ed6a095968ff1
daeho kim <daeho.kim@softwareinlife.com>
2021-07-20 19:44:44 +0900

    wtf

---
+++ src/main/java/com/example/userservice/entity/Member.java
@@ -0,0 +1,11 @@
+package com.example.userservice.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name = "abc")
+public class Member {
+
+    @Id
+    private String wtf;
+}
